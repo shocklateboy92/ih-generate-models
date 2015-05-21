768620b05fabd90500867a9805fc87fdc6b63de3
Lasath Fernando <kde@lasath.org>
2015-05-21 12:42:07 +1000

    Remove some dead code

--- mutation-ratios.cpp
+++ mutation-ratios.cpp
@@ -15,15 +15,6 @@ enum tri_nt_pos {
     right
 };
 
-//namespace args = std::placeholders;
-
-//double lookup_tri_nt(RunConfig config, seq_t tri_nt, char to_nt) {
-//    std::cerr << tri_nt;
-//    std::replace(tri_nt.begin(), tri_nt.end(), nt::any, to_nt);
-//    std::cerr << " looking up " << tri_nt << std::endl;
-//    return config.mutation_probs.at(tri_nt);
-//}
-
 // compare to MutationSpectrum.getTNProbability()
 double fetch_mutation_ratio(RunConfig config, std::string sequence,
                             std::size_t index, char to_nt) {
@@ -34,17 +25,6 @@ double fetch_mutation_ratio(RunConfig config, std::string sequence,
     assert(to_nt != nt::gap);
 
     assert(sequence.find(nt::gap) == std::string::npos); // don't support gaps yet
-
-    // if the to_nt is nt::any, we need to look up all permutations
-    //    std::vector<double> probs(TRACK.size());
-    //    if (tri_nt.find(nt::any) != std::string::npos) {
-    //        probs.reserve(TRACK.size());
-    //        std::copy_if(TRACK.begin(), TRACK.end(), probs.begin(),
-    //                     std::bind(&lookup_tri_nt,
-    //                               config,
-    //                               mutated_tri_nt,
-    //                               args::_1));
-    //    }
 
     double sum = 0;
     std::size_t matches = 0;
