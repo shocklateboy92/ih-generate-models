55b26aec833a10f81ede5037bac8c36184664e87
Lasath Fernando <kde@lasath.org>
2015-05-27 16:15:46 +1000

    Fix warning in fastareader

--- lib/fastareader/abstractcharsequence.h
+++ lib/fastareader/abstractcharsequence.h
@@ -9,17 +9,17 @@
  * AbstractCharSequence
  */
 class AbstractCharSequence
-	{
-	public:
-	    AbstractCharSequence();
-	    virtual ~AbstractCharSequence();
-	    virtual char at(int32_t index) const=0;
-	    virtual int32_t size() const=0;
-	    char operator[](int32_t index) const;
-	    virtual std::ostream&  print(std::ostream& out) const;
-	    virtual std::ostream&  print(std::ostream& out,int32_t beg) const;
-	    virtual std::ostream&  print(std::ostream& out,int32_t beg, int32_t end) const;
-	    virtual std::auto_ptr<std::string> toString() const;
-	};
+    {
+    public:
+        AbstractCharSequence();
+        virtual ~AbstractCharSequence();
+        virtual char at(int32_t index) const=0;
+        virtual int32_t size() const=0;
+        char operator[](int32_t index) const;
+        virtual std::ostream&  print(std::ostream& out) const;
+        virtual std::ostream&  print(std::ostream& out,int32_t beg) const;
+        virtual std::ostream&  print(std::ostream& out,int32_t beg, int32_t end) const;
+        virtual std::string toString() const;
+    };
 
 #endif
