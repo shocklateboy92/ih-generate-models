e11adb1f49e4ab053d2e066880dec7af04d36c25
Lasath Fernando <kde@lasath.org>
2015-06-01 16:01:02 +1000

    Remove dead code from main()

--- main.cpp
+++ main.cpp
@@ -27,36 +27,9 @@ int main(int argc, char *argv[]) {
 
     auto nodes = doc.select_nodes("//BlastOutput/BlastOutput_iterations/Iteration");
 
-//    using boost::adaptors::transform;
-
-    #define BIND_CONFIG(x) std::bind(x, globalConfig, std::placeholders::_1)
-
     auto i1 = readRepertoire(argv[2]);
     assert (nodes.size() == i1.size());
 
-//    std::vector<std::pair<const pugi::xpath_node&, FastaSequence>> pairs;
-//    for (std::size_t i = 0; i < nodes.size(); i++) {
-//        pairs.push_back({nodes[i], i1[i]});
-//    }
-
-//    // Parse the Iteration nodes
-//    auto r1 = transform(pairs, [](auto b) {return std::make_pair(parseBlastOutput(b.first), b.second);});
-//    auto r2 = transform(r1, [](auto b) -> SequenceInfo {
-//        return {
-//                b.second.name(),
-//                b.second.c_str(),
-//                b.first,
-//                // Calculate A Score from Blast Results
-//                calculateAScore(b.first)
-//        };
-//    });
-//    auto r3 = transform(r2, BIND_CONFIG(buildModel));
-//    auto r4 = transform(r3, do_viterbi);
-
-//    for (auto v : r4) {
-//        for (auto s : v)
-//            std::cout << s.name << std::endl;
-//    }
     tbb::concurrent_vector<std::vector<StateInfo>> vec(nodes.size());
 
     tbb::parallel_for(0ul, nodes.size(), [&](std::size_t i) {
